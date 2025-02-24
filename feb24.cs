2f078c37acd4eac3ed0ca3cbec71a248a476c941
JaisalNVal <jaisalm182@stemcollegiate.ca>
2025-02-24 14:02:52 -0700

    Create feb24.cs

---
+++ feb24.cs
@@ -0,0 +1,13 @@
+string name = "Bob";
+int messages = 3;
+decimal temperature = 34.4m;
+
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write("! You have ");
+Console.Write(messages);
+Console.Write(" messages in your inbox. The temperature is ");
+Console.Write(temperature);
+Console.Write(" celsius.");
+
+This is the solution to our code question.
