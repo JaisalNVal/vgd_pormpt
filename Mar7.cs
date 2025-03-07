cffdf15c038f1a63d145b1573a8a57298a1ccc40
JaisalNVal <jaisalm182@stemcollegiate.ca>
2025-03-07 10:12:40 -0700

    Update Mar7.cs

--- Mar7.cs
+++ Mar7.cs
@@ -1,1 +1,15 @@
+string firstName;
+firstName= "Krish";
+Console.WriteLine("Krish");
+string name = "Bob";
+int messages = 3;
+decimal temperature = 34.4m;
 
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write("! You have ");
+Console.Write(messages);
+Console.Write(" messages in your inbox. The temperature is ");
+Console.Write(temperature);
+Console.Write(" celsius.");
+Today I learned how to write 
